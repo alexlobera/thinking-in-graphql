914aa4bb038d2e65dc98f697f72b7a11776d01af
alexlbr <alex.lobera@gmail.com>
2017-08-15 16:43:08 +0100

    removed unnecessary code from coures model

--- src/mongoose/models/course.js
+++ src/mongoose/models/course.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
-const courseObject = {
+const courseSchema = new Schema({
   title: String,
   address: String,
   city: String,
@@ -12,25 +12,6 @@ const courseObject = {
   end_date: Date,
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date },
-}
-
-const course = new Schema(courseObject)
-
-const courseSchema = new Schema({
-  ...courseObject,
-  courses: [course]
-},{
-  toObject: {
-    transform: function (doc, ret) {
-      ret.id = ret._id
-      delete ret._id
-    }
-  },
-  toJSON: {
-    transform: function (doc, ret) {
-      delete ret._id;
-    }
-  }
 })
 
 export default mongoose.model('Course', courseSchema)
